db8f4d762271afdaf6fb24f7b9ae2767f20ece97
Ariana Kržan <ak1193@student.uni-lj.si>
2023-12-05 14:39:18 +0100

    layout, animation

---
+++ sd_1.6/webui-user.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set PYTHON="C:\Users\arian\AppData\Local\Programs\Python\Python310\python.exe"
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS= --no-half-vae --no-half --medvram --opt-sdp-attention
+
+call webui.bat
