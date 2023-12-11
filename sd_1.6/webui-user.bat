a9c352990fd565d57bda9080b4832de9117323c5
Ariana Kržan <ak1193@student.uni-lj.si>
2023-12-11 19:56:09 +0100

    ukazna vrstica, SDXL, besedilo

--- sd_1.6/webui-user.bat
+++ sd_1.6/webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON="C:\Users\arian\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --no-half-vae --no-half --medvram --opt-sdp-attention --xformers
+set COMMANDLINE_ARGS= --no-half-vae --no-half --lowvram --medvram --opt-sdp-attention --xformers
 
 call webui.bat
